25e832de3ed8b546c2ea86ffb4297a8e1bb48e70
OctopuSSX <vladik0701@gmail.com>
2021-06-12 20:04:57 +0300

    Fix timezone fuckery

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import "reflect-metadata";
 import Bot from "./src/Bot";
 import Logger, { LogLevel } from "./src/Logger";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 if(process.argv.includes('--debug')) {
     Logger.logLevel = LogLevel.DEBUG;
     Logger.log(LogLevel.DEBUG, "[DEBUG] LogLevel set to DEBUG");
